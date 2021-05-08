5c2db5fdbf33963b27e96ff9d4bcc33a21864607
Paula Cynthia <pcynthia013@gmail.com>
2021-05-08 20:08:36 -0300

    Security config

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,22 +10,21 @@ function App() {
       <NavBar />
       <div className="container">
         <h1 className="text-primary py-3">Dashboard de Vendas</h1>
-
         <div className="row px-3">
           <div className="col-sm-6">
             <h5 className="text-center text-secondary">Taxa de sucesso (%)</h5>
+
             <BarChart />
           </div>
           <div className="col-sm-6">
             <h5 className="text-center text-secondary">Todas as vendas</h5>
+            
             <DonutChart />
           </div>
         </div>
-
         <div className="py-3">
           <h2 className="text-primary">Todas as vendas</h2>
         </div>
-
         <DataTable />
       </div>
       <Footer />
