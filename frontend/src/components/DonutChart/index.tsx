ebb3fe88f999b23a7d58f62b3b7d0351f6ae0171
Paula Cynthia <pcynthia013@gmail.com>
2021-05-09 20:39:15 -0300

    First request

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,12 +1,39 @@
-import Chart from 'react-apexcharts'; 
+import axios from 'axios';
+import Chart from 'react-apexcharts';
+import { SaleSum } from 'types/sale';
+import { BASE_URL } from 'utils/requests';
+
+type ChartData = {
+    labels: string[];
+    series: number[];
+}
 
 const DonutChart = () => {
 
-    const mockData = {
+    // FORMA DATA
+    let chartData: ChartData = { labels: [], series: [] };
+
+    // Foi utilizado let e não const porque iremos alterar o valor do chartData com os dados do backend
+
+    /* const mockData = {
         series: [477138, 499928, 444867, 220426, 473088],
         labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-    }
-    
+    } */
+
+    axios.get(`${BASE_URL}/sales/amount-by-seller`)
+        // Requisição assíncrona: quando você faz uma requisição na web no js essa operação é assíncrona
+        // Ou seja, ela será chamada a sua aplicação continua rodando quando a resposta da api retornar, aí sim você executa alguma coisa
+        // Para você executar essa coisa depois que vier a resposta da requisição 
+        // O ponto then será definido dentro dele uma função para ser executada quando a resposta da API chegar e chegar com sucesso
+        .then(response => { // Then tem como argumento a resposta e como é só um argumento não precisa de parênteses
+            const data = response.data as SaleSum[];
+            const myLabels = data.map(x => x.sellerName);
+            const mySeries = data.map(x => x.sum);
+
+            chartData = { labels: myLabels, series: mySeries };
+            console.log(chartData);
+        });
+
     const options = {
         legend: {
             show: true
@@ -14,9 +41,9 @@ const DonutChart = () => {
     }
 
     return (
-        <Chart 
-            options= {{...options, labels: mockData.labels}}
-            series={mockData.series}
+        <Chart
+            options={{ ...options, labels: chartData.labels }}
+            series={chartData.series}
             type="donut"
             height="240"
 
