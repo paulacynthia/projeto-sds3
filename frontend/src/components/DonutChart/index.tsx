3061be51b912b2719012d3163770f0ae3939d665
Paula Cynthia <pcynthia013@gmail.com>
2021-05-09 21:06:31 -0300

    DonutChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts';
 import { SaleSum } from 'types/sale';
 import { BASE_URL } from 'utils/requests';
@@ -10,29 +11,19 @@ type ChartData = {
 
 const DonutChart = () => {
 
-    // FORMA DATA
-    let chartData: ChartData = { labels: [], series: [] };
+    const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] });
 
-    // Foi utilizado let e não const porque iremos alterar o valor do chartData com os dados do backend
+    useEffect(() => {
+        axios.get(`${BASE_URL}/sales/amount-by-seller`)
+            .then(response => {
+                const data = response.data as SaleSum[];
+                const myLabels = data.map(x => x.sellerName);
+                const mySeries = data.map(x => x.sum);
 
-    /* const mockData = {
-        series: [477138, 499928, 444867, 220426, 473088],
-        labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-    } */
+                setChartData({ labels: myLabels, series: mySeries });
 
-    axios.get(`${BASE_URL}/sales/amount-by-seller`)
-        // Requisição assíncrona: quando você faz uma requisição na web no js essa operação é assíncrona
-        // Ou seja, ela será chamada a sua aplicação continua rodando quando a resposta da api retornar, aí sim você executa alguma coisa
-        // Para você executar essa coisa depois que vier a resposta da requisição 
-        // O ponto then será definido dentro dele uma função para ser executada quando a resposta da API chegar e chegar com sucesso
-        .then(response => { // Then tem como argumento a resposta e como é só um argumento não precisa de parênteses
-            const data = response.data as SaleSum[];
-            const myLabels = data.map(x => x.sellerName);
-            const mySeries = data.map(x => x.sum);
-
-            chartData = { labels: myLabels, series: mySeries };
-            console.log(chartData);
-        });
+            });
+    }, []);
 
     const options = {
         legend: {
